ad94580b0310af62a4625629d558556e2b20ca2b
fadaeixlii <mohammad4252@gmail.com>
2025-08-25 11:28:26 +0330

    Complete auth decorator with swagger for handle auth

--- src/modules/auth/decorators/auth.decorator.ts
+++ src/modules/auth/decorators/auth.decorator.ts
@@ -1,10 +1,31 @@
-import { SetMetadata } from '@nestjs/common';
+import { SetMetadata, applyDecorators, CustomDecorator } from '@nestjs/common';
 import { AuthOptions, AuthType } from '../types/auth.type';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 export const AUTH_OPTIONS_KEY = 'auth:options';
 
-export const Auth = (options: AuthOptions = { type: AuthType.ADMIN_USER }) =>
-  SetMetadata(AUTH_OPTIONS_KEY, options);
+export const Auth = (options: AuthOptions = { type: AuthType.ADMIN_USER }) => {
+  const decorators: Array<
+    ClassDecorator | MethodDecorator | CustomDecorator<string>
+  > = [SetMetadata(AUTH_OPTIONS_KEY, options)];
+
+  if (options.type === AuthType.ADMIN_USER) {
+    decorators.push(
+      ApiBearerAuth('jwt'),
+      ApiUnauthorizedResponse({ description: 'Invalid or missing JWT' }),
+    );
+  }
+
+  if (options.type === AuthType.ADMIN_USER) {
+    decorators.push(ApiForbiddenResponse({ description: 'Admins only' }));
+  }
+
+  return applyDecorators(...decorators);
+};
 
 export const Public = () => Auth({ type: AuthType.PUBLIC });
 
